cf25f3ca60904287102ebffd6a7a3f4fa102b835
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2017-04-04 00:34:46 -0300

    Old aliases related file and oh-my-zsh cleanup

---
+++ os/symlink-setup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+copy_custom_theme() {
+
+  print_in_purple "\n • Copy custom ZSH theme to proper folder\n\n"
+  source ../zsh/zsh_theme
+  print_success ".zsh-theme file copied!"
+
+}
+
+create_symlinks() {
+
+    declare -a FILES_TO_SYMLINK=(
+
+        "shell/aliases/aliases"
+        "shell/bash_profile"
+        "shell/bash_prompt"
+        "shell/bashrc"
+
+        "zsh/zsh_theme"
+        "zsh/zshrc"
+
+        "git/gitconfig"
+        "git/gitignore"
+        "git/extras"
+
+        "tmux/tmux.conf"
+
+        "vim/vimrc"
+
+    )
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    skip_questions "$@" \
+        && skipQuestions=true
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    for i in "${FILES_TO_SYMLINK[@]}"; do
+
+        sourceFile="$(cd .. && pwd)/$i"
+        targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
+
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
+
+        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+        else
+
+            if ! $skipQuestions; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+
+                    rm -rf "$targetFile"
+
+                    execute \
+                        "ln -fs $sourceFile $targetFile" \
+                        "$targetFile → $sourceFile"
+
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            fi
+
+        fi
+
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    copy_custom_theme
+    print_in_purple "\n • Create symbolic links\n\n"
+    create_symlinks "$@"
+}
+
+main "$@"
