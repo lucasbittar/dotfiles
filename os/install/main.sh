7a82ff9e459a2aac6c8e2597b4ffb01045332d84
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2017-10-01 17:34:44 -0300

    Add ZSH plugings

--- os/install/main.sh
+++ os/install/main.sh
@@ -21,4 +21,13 @@ vim +PluginInstall +qall
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+print_in_purple "\n   ZSH Plugins\n\n"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 printf "\n"
